ba990887a9d03607d41fe904907864e02c77929f
federicolepera <federico.lepera@kiratech.it>
2026-02-04 19:01:56 +0100

    feat: add dynamic SLI error rate and burn rate rules to PrometheusRule generation

--- internal/prometheus/prometheus_rule_generator.go
+++ internal/prometheus/prometheus_rule_generator.go
@@ -2,7 +2,6 @@ package prometheus
 
 import (
 	"fmt"
-	"strings"
 
 	observabilityv1alpha1 "github.com/federicolepera/slok/api/v1alpha1"
 	monitoringv1 "github.com/prometheus-operator/prometheus-operator/pkg/apis/monitoring/v1"
@@ -50,6 +49,173 @@ func CreatePrometheusRule(sloName string, sloNamespace string, objective observa
 	prometheusRule.Spec.Groups = append(prometheusRule.Spec.Groups, monitoringv1.RuleGroup{
 		Name: fmt.Sprintf("slok.%s.%s", sloName, objectiveName),
 	})
+
+
+	prometheusRule.Spec.Groups[0].Rules = append(prometheusRule.Spec.Groups[0].Rules, monitoringv1.Rule{
+		Record: fmt.Sprint("slok:sli_error_rate:5m"),
+		Expr: intstr.FromString(fmt.Sprintf("(sum(rate(%s[5m])) OR (sum(rate(%s[5m])) * 0)) / clamp_min(sum(rate(%s[5m])), 1e-12)", objective.Sli.Query.ErrorQuery, objective.Sli.Query.TotalQuery, objective.Sli.Query.TotalQuery)),
+		Labels: map[string]string{
+			"slo_name":       sloName,
+			"slo_namespace":  sloNamespace,
+			"objective_name": objectiveName,
+			"slok_window":     "5m",
+		},
+	})
+
+	prometheusRule.Spec.Groups[0].Rules = append(prometheusRule.Spec.Groups[0].Rules, monitoringv1.Rule{
+		Record: fmt.Sprint("slok:sli_error_rate:1h"),
+		Expr: intstr.FromString(fmt.Sprintf("(sum(rate(%s[1h])) OR (sum(rate(%s[1h])) * 0)) / clamp_min(sum(rate(%s[1h])), 1e-12)", objective.Sli.Query.ErrorQuery, objective.Sli.Query.TotalQuery, objective.Sli.Query.TotalQuery)),
+		Labels: map[string]string{
+			"slo_name":       sloName,
+			"slo_namespace":  sloNamespace,
+			"objective_name": objectiveName,
+			"slok_window":     "1h",
+		},
+	})
+
+	prometheusRule.Spec.Groups[0].Rules = append(prometheusRule.Spec.Groups[0].Rules, monitoringv1.Rule{
+		Record: fmt.Sprint("slok:sli_error_rate:6h"),
+		Expr: intstr.FromString(fmt.Sprintf("(sum(rate(%s[6h])) OR (sum(rate(%s[6h])) * 0)) / clamp_min(sum(rate(%s[6h])), 1e-12)", objective.Sli.Query.ErrorQuery, objective.Sli.Query.TotalQuery, objective.Sli.Query.TotalQuery)),
+		Labels: map[string]string{
+			"slo_name":       sloName,
+			"slo_namespace":  sloNamespace,
+			"objective_name": objectiveName,
+			"slok_window":     "6h",
+		},
+	})
+
+	prometheusRule.Spec.Groups[0].Rules = append(prometheusRule.Spec.Groups[0].Rules, monitoringv1.Rule{
+		Record: fmt.Sprint("slok:sli_error_rate:3d"),
+		Expr: intstr.FromString(fmt.Sprintf("(sum(rate(%s[3d])) OR (sum(rate(%s[3d])) * 0)) / clamp_min(sum(rate(%s[3d])), 1e-12)", objective.Sli.Query.ErrorQuery, objective.Sli.Query.TotalQuery, objective.Sli.Query.TotalQuery)),
+		Labels: map[string]string{
+			"slo_name":       sloName,
+			"slo_namespace":  sloNamespace,
+			"objective_name": objectiveName,
+			"slok_window":     "3d",
+		},
+	})
+
+	prometheusRule.Spec.Groups[0].Rules = append(prometheusRule.Spec.Groups[0].Rules, monitoringv1.Rule{
+		Record: fmt.Sprint("slok:sli_error_rate:7d"),
+		Expr: intstr.FromString(fmt.Sprintf("(sum(rate(%s[7d])) OR (sum(rate(%s[7d])) * 0)) / clamp_min(sum(rate(%s[7d])), 1e-12)", objective.Sli.Query.ErrorQuery, objective.Sli.Query.TotalQuery, objective.Sli.Query.TotalQuery)),
+		Labels: map[string]string{
+			"slo_name":       sloName,
+			"slo_namespace":  sloNamespace,
+			"objective_name": objectiveName,
+			"slok_window":     "7d",
+		},
+	})
+
+	prometheusRule.Spec.Groups[0].Rules = append(prometheusRule.Spec.Groups[0].Rules, monitoringv1.Rule{
+		Record: fmt.Sprint("slok:sli_error_rate:30d"),
+		Expr: intstr.FromString(fmt.Sprintf("(sum(rate(%s[30d])) OR (sum(rate(%s[30d])) * 0)) / clamp_min(sum(rate(%s[30d])), 1e-12)", objective.Sli.Query.ErrorQuery, objective.Sli.Query.TotalQuery, objective.Sli.Query.TotalQuery)),
+		Labels: map[string]string{
+			"slo_name":       sloName,
+			"slo_namespace":  sloNamespace,
+			"objective_name": objectiveName,
+			"slok_window":     "30d",
+		},
+	})
+
+	prometheusRule.Spec.Groups[0].Rules = append(prometheusRule.Spec.Groups[0].Rules, monitoringv1.Rule{
+		Record: fmt.Sprint("slok:objective_target"),
+		Expr: intstr.FromString(fmt.Sprintf("vector(%g)", objective.Target/100)),
+		Labels: map[string]string{
+			"slo_name":       sloName,
+			"slo_namespace":  sloNamespace,
+			"objective_name": objectiveName,
+			"slok_window":     "30d",
+		},
+	})
+
+	prometheusRule.Spec.Groups[0].Rules = append(prometheusRule.Spec.Groups[0].Rules, monitoringv1.Rule{
+		Record: fmt.Sprint("slok:error_budget_target"),
+		Expr: intstr.FromString(fmt.Sprintf("vector(%g)", 1-objective.Target/100)),
+		Labels: map[string]string{
+			"slo_name":       sloName,
+			"slo_namespace":  sloNamespace,
+			"objective_name": objectiveName,
+			"slok_window":     "30d",
+		},
+	})
+
+	prometheusRule.Spec.Groups[0].Rules = append(prometheusRule.Spec.Groups[0].Rules, monitoringv1.Rule{
+		Record: fmt.Sprint("slok:error_budget_target"),
+		Expr: intstr.FromString(fmt.Sprintf("vector(%g)", 1-objective.Target/100)),
+		Labels: map[string]string{
+			"slo_name":       sloName,
+			"slo_namespace":  sloNamespace,
+			"objective_name": objectiveName,
+			"slok_window":     "30d",
+		},
+	})
+
+	prometheusRule.Spec.Groups[0].Rules = append(prometheusRule.Spec.Groups[0].Rules, monitoringv1.Rule{
+		Record: fmt.Sprint("slok:burn_rate:5m"),
+		Expr: intstr.FromString(fmt.Sprintf("slok:sli_error_rate:5m{slo_name=\"%s\", slo_namespace=\"%s\", objective_name=\"%s\"} / on (slo_name, slo_namespace, objective_name) slok:error_budget_target{slo_name=\"%s\", slo_namespace=\"%s\", objective_name=\"%s\"}", sloName, sloNamespace, objectiveName, sloName, sloNamespace, objectiveName)),
+		Labels: map[string]string{
+			"slo_name":       sloName,
+			"slo_namespace":  sloNamespace,
+			"objective_name": objectiveName,
+			"slok_window":     "5m",
+		},
+	})
+
+	prometheusRule.Spec.Groups[0].Rules = append(prometheusRule.Spec.Groups[0].Rules, monitoringv1.Rule{
+		Record: fmt.Sprint("slok:burn_rate:1h"),
+		Expr: intstr.FromString(fmt.Sprintf("slok:sli_error_rate:1h{slo_name=\"%s\", slo_namespace=\"%s\", objective_name=\"%s\"} / on (slo_name, slo_namespace, objective_name) slok:error_budget_target{slo_name=\"%s\", slo_namespace=\"%s\", objective_name=\"%s\"}", sloName, sloNamespace, objectiveName, sloName, sloNamespace, objectiveName)),
+		Labels: map[string]string{
+			"slo_name":       sloName,
+			"slo_namespace":  sloNamespace,
+			"objective_name": objectiveName,
+			"slok_window":     "1h",
+		},
+	})
+
+	prometheusRule.Spec.Groups[0].Rules = append(prometheusRule.Spec.Groups[0].Rules, monitoringv1.Rule{
+		Record: fmt.Sprint("slok:burn_rate:6h"),
+		Expr: intstr.FromString(fmt.Sprintf("slok:sli_error_rate:6h{slo_name=\"%s\", slo_namespace=\"%s\", objective_name=\"%s\"} / on (slo_name, slo_namespace, objective_name) slok:error_budget_target{slo_name=\"%s\", slo_namespace=\"%s\", objective_name=\"%s\"}", sloName, sloNamespace, objectiveName, sloName, sloNamespace, objectiveName)),
+		Labels: map[string]string{
+			"slo_name":       sloName,
+			"slo_namespace":  sloNamespace,
+			"objective_name": objectiveName,
+			"slok_window":     "6h",
+		},
+	})
+
+	prometheusRule.Spec.Groups[0].Rules = append(prometheusRule.Spec.Groups[0].Rules, monitoringv1.Rule{
+		Record: fmt.Sprint("slok:burn_rate:3d"),
+		Expr: intstr.FromString(fmt.Sprintf("slok:sli_error_rate:3d{slo_name=\"%s\", slo_namespace=\"%s\", objective_name=\"%s\"} / on (slo_name, slo_namespace, objective_name) slok:error_budget_target{slo_name=\"%s\", slo_namespace=\"%s\", objective_name=\"%s\"}", sloName, sloNamespace, objectiveName, sloName, sloNamespace, objectiveName)),
+		Labels: map[string]string{
+			"slo_name":       sloName,
+			"slo_namespace":  sloNamespace,
+			"objective_name": objectiveName,
+			"slok_window":     "3d",
+		},
+	})
+
+	prometheusRule.Spec.Groups[0].Rules = append(prometheusRule.Spec.Groups[0].Rules, monitoringv1.Rule{
+		Record: fmt.Sprint("slok:burn_rate:7d"),
+		Expr: intstr.FromString(fmt.Sprintf("slok:sli_error_rate:7d{slo_name=\"%s\", slo_namespace=\"%s\", objective_name=\"%s\"} / on (slo_name, slo_namespace, objective_name) slok:error_budget_target{slo_name=\"%s\", slo_namespace=\"%s\", objective_name=\"%s\"}", sloName, sloNamespace, objectiveName, sloName, sloNamespace, objectiveName)),
+		Labels: map[string]string{
+			"slo_name":       sloName,
+			"slo_namespace":  sloNamespace,
+			"objective_name": objectiveName,
+			"slok_window":     "7d",
+		},
+	})
+
+	prometheusRule.Spec.Groups[0].Rules = append(prometheusRule.Spec.Groups[0].Rules, monitoringv1.Rule{
+		Record: fmt.Sprint("slok:burn_rate:30d"),
+		Expr: intstr.FromString(fmt.Sprintf("slok:sli_error_rate:30d{slo_name=\"%s\", slo_namespace=\"%s\", objective_name=\"%s\"} / on (slo_name, slo_namespace, objective_name) slok:error_budget_target{slo_name=\"%s\", slo_namespace=\"%s\", objective_name=\"%s\"}", sloName, sloNamespace, objectiveName, sloName, sloNamespace, objectiveName)),
+		Labels: map[string]string{
+			"slo_name":       sloName,
+			"slo_namespace":  sloNamespace,
+			"objective_name": objectiveName,
+			"slok_window":     "30d",
+		},
+	})
+
 	if objective.Alerting.BudgetErrorAlerts.Enabled {
 		// Creation of default budget alerts
 		prometheusRule.Spec.Groups[0].Rules = append(prometheusRule.Spec.Groups[0].Rules, monitoringv1.Rule{
@@ -83,36 +249,56 @@ func CreatePrometheusRule(sloName string, sloNamespace string, objective observa
 	}
 
 	if objective.Alerting.BurnRateAlerts.Enabled {
-		// Creation of default burn rate alerts based on presets
-		for _, preset := range defaultBurnRatePresets {
-			successLong := strings.ReplaceAll(objective.Sli.Query.Success, "$window", preset.LongWindow)
-			totalLong := strings.ReplaceAll(objective.Sli.Query.Total, "$window", preset.LongWindow)
-			successShort := strings.ReplaceAll(objective.Sli.Query.Success, "$window", preset.ShortWindow)
-			totalShort := strings.ReplaceAll(objective.Sli.Query.Total, "$window", preset.ShortWindow)
+		prometheusRule.Spec.Groups = append(prometheusRule.Spec.Groups, monitoringv1.RuleGroup{
+			Name: fmt.Sprintf("slok.%s.%s-burnRateAlerts", sloName, objectiveName),
+		})
+		prometheusRule.Spec.Groups[1].Rules = append(prometheusRule.Spec.Groups[1].Rules, monitoringv1.Rule{
+			Alert: fmt.Sprintf("Objective: %s SLOBurnRateHigh - Critical", objectiveName),
+			Expr: intstr.FromString(fmt.Sprintf("slok:burn_rate:5m{slo_name=\"%s\", slo_namespace=\"%s\", objective_name=\"%s\"} > 14 AND slok:burn_rate:1h{slo_name=\"%s\", slo_namespace=\"%s\", objective_name=\"%s\"} > 14", sloName, sloNamespace, objectiveName, sloName, sloNamespace, objectiveName)),
+			Labels: map[string]string{
+				"slo_name":       sloName,
+				"slo_namespace":  sloNamespace,
+				"objective_name": objectiveName,
+				"slok_window":     "5m",
+				"severity":      "critical",
+			},
+		})
 
-			expr := fmt.Sprintf(
-				"(1 - (%s / %s)) / (1 - (%g / 100)) > %g AND (1 - (%s / %s)) / (1 - (%g / 100)) > %g",
-				successLong, totalLong, objective.Target, preset.BurnRate,
-				successShort, totalShort, objective.Target, preset.BurnRate,
-			)
+		prometheusRule.Spec.Groups[1].Rules = append(prometheusRule.Spec.Groups[1].Rules, monitoringv1.Rule{
+			Alert: fmt.Sprintf("Objective: %s SLOBurnRateHigh - Degraded", objectiveName),
+			Expr: intstr.FromString(fmt.Sprintf("slok:burn_rate:1h{slo_name=\"%s\", slo_namespace=\"%s\", objective_name=\"%s\"} > 6 AND slok:burn_rate:6h{slo_name=\"%s\", slo_namespace=\"%s\", objective_name=\"%s\"} > 6", sloName, sloNamespace, objectiveName, sloName, sloNamespace, objectiveName)),
+			Labels: map[string]string{
+				"slo_name":       sloName,
+				"slo_namespace":  sloNamespace,
+				"objective_name": objectiveName,
+				"slok_window":     "6h",
+				"severity":      "warning",
+			},
+		})
 
-			prometheusRule.Spec.Groups[0].Rules = append(prometheusRule.Spec.Groups[0].Rules, monitoringv1.Rule{
-				Alert: fmt.Sprintf("SLOBurnRate%s", preset.AlertSuffix),
-				Expr:  intstr.FromString(expr),
-				For:   monitoringv1.DurationPointer(preset.For),
-				Labels: map[string]string{
-					"severity":  preset.Severity,
-					"slo":       sloName,
-					"objective": objectiveName,
-				},
-				Annotations: map[string]string{
-					"summary": fmt.Sprintf(
-						"%s/%s: burn rate >%gx over %s/%s window",
-						sloName, objectiveName, preset.BurnRate, preset.LongWindow, preset.ShortWindow,
-					),
-				},
-			})
-		}
+		prometheusRule.Spec.Groups[1].Rules = append(prometheusRule.Spec.Groups[1].Rules, monitoringv1.Rule{
+			Alert: fmt.Sprintf("Objective: %s SLOBurnRateHigh - Warningn", objectiveName),
+			Expr: intstr.FromString(fmt.Sprintf("slok:burn_rate:6h{slo_name=\"%s\", slo_namespace=\"%s\", objective_name=\"%s\"} > 1 AND slok:burn_rate:3d{slo_name=\"%s\", slo_namespace=\"%s\", objective_name=\"%s\"} > 1", sloName, sloNamespace, objectiveName, sloName, sloNamespace, objectiveName)),
+			Labels: map[string]string{
+				"slo_name":       sloName,
+				"slo_namespace":  sloNamespace,
+				"objective_name": objectiveName,
+				"slok_window":     "3d",
+				"severity":      "warning",
+			},
+		})
+
+		prometheusRule.Spec.Groups[1].Rules = append(prometheusRule.Spec.Groups[1].Rules, monitoringv1.Rule{
+			Alert: fmt.Sprintf("Objective: %s ErrorBudget Finished - Violated", objectiveName),
+			Expr: intstr.FromString(fmt.Sprintf("slok:sli_error_rate:%s{slo_name=\"%s\", slo_namespace=\"%s\", objective_name=\"%s\"} > slok:error_budget_target{slo_name=\"%s\", slo_namespace=\"%s\", objective_name=\"%s\"}", objective.Window, sloName, sloNamespace, objectiveName, sloName, sloNamespace, objectiveName)),
+			Labels: map[string]string{
+				"slo_name":       sloName,
+				"slo_namespace":  sloNamespace,
+				"objective_name": objectiveName,
+				"slok_window":     objective.Window,
+				"severity":      "warning",
+			},
+		})
 	}
 	return prometheusRule, nil
 }
